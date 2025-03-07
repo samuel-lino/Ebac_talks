622ae4c37f8d4ac624c339726a573037c8d6a617
samuel-eduardo <samuellino2006@gmail.com>
2025-03-07 16:37:41 -0300

    finalizado

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,30 @@
 AOS.init();
+
+
+const datadoevento = new Date("Mar 23, 2025 19:00:00");
+const timestampdoevento = datadoevento.getTime();
+
+const contador = setInterval(function(){
+    const agora = new Date();
+    const timestampatual = agora.getTime();
+    
+    const distanciadoevento = timestampdoevento - timestampatual;
+
+    const diasemMS = 1000 * 60 * 60 * 24;
+    const horasemMS = 1000 * 60 * 60;
+    const minemMS = 1000 * 60;
+    const segemMS = 1000
+    
+    const diasateoevento = Math.floor(distanciadoevento / diasemMS);
+    const horasateoevento =Math.floor((distanciadoevento % diasemMS/horasemMS));
+    const minutosateoevento = Math.floor((distanciadoevento %horasemMS)/ minemMS);
+    const segundosateoevento = Math.floor((distanciadoevento%minemMS)/segemMS);
+
+    document.getElementById('contador').innerHTML = `${diasateoevento}d ${horasateoevento}h ${minutosateoevento}m ${segundosateoevento}s`
+
+    if(distanciadoevento < 0){
+        clearInterval(contador);
+        document.getElementById('contador').innerHTML = 'Evento expirou'
+    }
+
+}, 1000);
